7267524602af959ab85a4cf903a03388376df1bd
Pierre Slagbrand <pierre.slagbrand@hotmail.com>
2023-10-12 07:59:02 +0000

    remove secret key and set debug

--- sweraka/settings.py
+++ sweraka/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wdkb^8k0@9v^365&hi=&etl_0poc%kbk9jb7pj00+72oeypcy='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'sweraka-6341db077d7b.herokuapp.com',
